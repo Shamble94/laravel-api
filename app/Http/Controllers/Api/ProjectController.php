d1a835fb4efce0b253c7303de2ce85f4b63b1b71
Shamble94 <fratus.nicholas@gmail.com>
2024-03-01 20:25:03 +0100

    Ho provato a cambiare nome all'oggetto technology ma la chiamata riceve un array vuoto in qualsiasi caso

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,7 +11,7 @@ class ProjectController extends Controller
 {
     public function index(){
 
-        $projects = Project::with(["type", "technology"])->paginate(4);
+        $projects = Project::with(["type", "technologies"])->paginate(4);
         
         return response()->json([
             "success"  => true,
